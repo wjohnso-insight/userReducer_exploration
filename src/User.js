44cbebe7fd31fea7dcfa63d6647e4c9f0489e82b
Will <will.johnston@insight.com>
2021-02-26 16:17:57 -0500

    Updated style
    1. Updated style

--- src/User.js
+++ src/User.js
@@ -1,4 +1,4 @@
-import React, { useReducer} from 'react'
+import React, { useReducer, useRef} from 'react'
 import styled from 'styled-components'
 
 const ComponentWrapper = styled.div`
@@ -11,19 +11,29 @@ const ComponentWrapper = styled.div`
     margin-top: 5%;
     flex-direction: column;
 `
+const Controls = styled.div`
+    display: flex;
+`
+const Input = styled.input`
+    margin: 5px;
+    font-size: 18pt;
+`
 
 export default function User({user}) {
 
     function reducer(state, action){
         switch (action.type) {
             case 'updateFirstName':
-                return{...state, firstName: 'Will'}         
+                return{...state, firstName: firstNameInputRef.current.value} //Spread state & update firstName property        
             case 'updateLastName':
-                return{...state, lastName: 'Johnston'}
+                return{...state, lastName: lastNameInputRef.current.value} //Spread state & update lastName property
             default:
                 throw new Error('Action not supported...')
         }
     }
+
+    const firstNameInputRef = useRef(null);
+    const lastNameInputRef = useRef(null);
     const initialState = {firstName: user.firstName, lastName: user.lastName};
     const [ state, dispatch ] = useReducer(reducer, initialState)
     
@@ -31,9 +41,21 @@ export default function User({user}) {
 
     return (
         <ComponentWrapper>
-            <p>{userName}</p>
-            <button onClick={() => dispatch({type: 'updateFirstName'})}>First Name</button>
-            <button onClick={() => dispatch({type: 'updateLastName'})}>Last Name</button>
+            <p style={{'fontWeight': 'bolder', 'fontSize' : '24pt'}}>{userName}</p>
+            <Controls>
+                <Input 
+                    type='text'
+                    placeholder='Update first name'
+                    ref={firstNameInputRef}
+                    onChange={() => dispatch({type: 'updateFirstName'})}
+                ></Input>
+                <Input 
+                    type='text'
+                    placeholder='Update last name'
+                    ref={lastNameInputRef}
+                    onChange={() => dispatch({type: 'updateLastName'})}
+                ></Input>
+            </Controls>
         </ComponentWrapper>
     )
 }
